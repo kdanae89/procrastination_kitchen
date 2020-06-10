76e20063e702cab2d108f59ee4e683440a8edb83
kdanae89 <kayliedanae@hotmail.com>
2020-06-10 11:09:25 -0700

    adds search a little styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import Axios from 'axios';
 import Header from './Header';
 import IngredientsSearch from './IngredientsSearch';
 
-const App = () => {
-  return (
-    <div className="container">
-      <Header />
-      <IngredientsSearch />
-    </div>
-  );
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+
+  render () {
+    return (
+      <div className="container">
+        <Header />
+        <IngredientsSearch
+          onSubmit={this.onSearchSubmit}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/IngredientsSearch.js
+++ src/components/IngredientsSearch.js
@@ -1,17 +1,33 @@
 import React from 'react';
 
 class IngredientsSearch extends React.Component {
+  state = { term: '' };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term)
+  }
+
   render () {
     return (
       <div>
-        <form style={{
-          display: "flex",
-          justifyContent: "space-evenly",
-          marginTop: "1em"
-        }}>
-          <div className="field">
+        <form
+          onSubmit={this.onFormSubmit}
+          style={{ marginTop: "1em" }}
+        >
+          <div
+            className="field"
+            style={{
+              display: "flex",
+              justifyContent: "center"
+          }}>
             <label>What's in your pantry?</label>
-            <input type="text" />
+            <input
+              type="text"
+              style={{ width: '50%'}}
+              onChange={(e) => this.setState({ term: e.target.value})}
+              value={this.state.term}
+            />
           </div>
         </form>
       </div>
